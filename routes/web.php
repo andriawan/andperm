356289488b1063840b71ba9336103043af3338ca
Muhammad Irwan Andriawan <irwan@andriawan.com>
2020-10-14 23:14:29 +0700

    add new routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/home', function () {
+	dump(Auth::user());
+    return view('dashboard');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
